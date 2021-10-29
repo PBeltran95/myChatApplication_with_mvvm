694fa29367ec64a933d0a3f3617e6b5459d60dca
Pablo <86982267+PBeltran95@users.noreply.github.com>
2021-10-29 19:01:34 -0300

    Update AppConstants.kt

--- app/src/main/java/ar/com/example/chatExample/application/AppConstants.kt
+++ app/src/main/java/ar/com/example/chatExample/application/AppConstants.kt
@@ -4,7 +4,7 @@ object AppConstants {
 
     const val BASE_URL = "https://fcm.googleapis.com"
     const val SERVER_KEY =
-        "AAAAfCeLz5Q:APA91bHIFeMqy0St0iuFN62REJtBHEE9OrBPV1PLshiyFTVAyaPv0qN7SXp7pg6EboB3f4l3L76MUFsc4jH66ccrw-2EJTNz183PXV-e88vBPpyjNEUE2G2q47U9PxqZEM9d0kcFWoXT"
+        "serverKey"
     const val CONTENT_TYPE = "application/json"
 
     const val TOPIC = "/topics"
